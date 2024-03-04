4c01b51b1fb090c12edcb2b255e5549410155317
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-03-04 21:42:50 +0200

    upd

--- js/task-4.js
+++ js/task-4.js
@@ -14,9 +14,9 @@ function getShippingCost(country) {
             price = 120;
             break;
         default:
-           return `Shipping to ${country} will cost ${price} credits`;
+            return "Sorry, there is no delivery to your country";
     }
-    return "Sorry, there is no delivery to your country";
+    return `Shipping to ${country} will cost ${price} credits`;
 }
 
 
